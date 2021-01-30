d61d9d55e5f8a0e280d74ed6ddd54491251fc0e4
Isaac <fallingpizza11@gmail.com>
2021-01-29 22:39:49 -0800

    capt'n hook

--- src/page.tsx
+++ src/page.tsx
@@ -1,24 +1,60 @@
-import React from "react"
-import {Dialog, Header, Input} from "@fluentui/react-northstar"
+import React, { useState } from "react"
+import { Dialog, Header, Input, Form, Button, Grid } from "@fluentui/react-northstar"
+
 
 
 function Page() {
-    return (
+  const [open, setOpen] = useState(true)
+
+  const formFields = [
+    {
+      label: "Enter Your Name",
+      name: "name",
+      id: "name-input",
+      key: "name",
+      required: true,
+      control: {
+        as: Input
+      }
+    },
+    {
+      label: "Enter Room Number",
+      name: "roomNumber",
+      id: "number-input",
+      key: "room-number",
+      required: true,
+      control: {
+        as: Input
+      }
+    },
+    {
+      control: {
+        as: Button,
+        content: 'Submit'
+      },
+      key: 'submit'
+    }
+  ]
+
+  return (
     <div id='page'>
-        <Header content="OnliClass" align='center'/>
-        <div id='content'>
-            <Dialog 
-                content={
-
-                        <Input label="Enter Your Name" />
-
-                }
-                open={true}
+      <Header content="OnliClass" align='center' />
+      <div id='content'>
+        <Dialog
+          content={
+            <Form
+              fields={formFields}
+              onSubmit={() => setOpen(false)}
             />
-            <p>this is content</p>
-        </div>
+          }
+          open={open}
+          trapFocus={true}
+        />
+        <Grid />
+        <p>this is content</p>
+      </div>
     </div>
-    )
+  )
 }
 
 export default Page
